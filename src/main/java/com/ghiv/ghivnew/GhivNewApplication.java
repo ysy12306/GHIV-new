be18ba2809ade91be9c412a3854b511af28740ee
yue sy <1731719956@qq.com>
2024-10-08 18:37:46 +0800

    Initial commit

---
+++ src/main/java/com/ghiv/ghivnew/GhivNewApplication.java
@@ -0,0 +1,13 @@
+package com.ghiv.ghivnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhivNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhivNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghiv/ghivnew/GhivNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghiv.ghivnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhivNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
