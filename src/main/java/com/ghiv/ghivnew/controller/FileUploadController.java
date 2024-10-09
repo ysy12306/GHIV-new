6d6c4830743a916b989944764a9baa8e872a9007
yue sy <1731719956@qq.com>
2024-10-09 12:22:15 +0800

    test

--- src/main/java/com/ghiv/ghivnew/controller/FileUploadController.java
+++ src/main/java/com/ghiv/ghivnew/controller/FileUploadController.java
@@ -1,7 +1,31 @@
 package com.ghiv.ghivnew.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @RestController
 public class FileUploadController {
+    @PostMapping("/upload")
+    public String up(String filename, MultipartFile file, HttpServletRequest request) throws IOException {
+
+        String path = request.getServletContext().getRealPath("/upload");
+        saveFile(file, path);
+
+        return "File uploaded successfully";
+    }
+
+    private void saveFile(MultipartFile file, String path) throws IOException {
+        File dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        File pfile = new File(path + file.getOriginalFilename());
+        file.transferTo(pfile);
+    }
 }
