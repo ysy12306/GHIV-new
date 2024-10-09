d9c575c85608ce94cc19238e73d0c2346d0629ef
yue sy <1731719956@qq.com>
2024-10-09 17:04:59 +0800

    test

--- src/main/java/com/ghiv/ghivnew/controller/testcontroller.java
+++ src/main/java/com/ghiv/ghivnew/controller/testcontroller.java
@@ -1,11 +1,12 @@
 package com.ghiv.ghivnew.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class testcontroller {
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String test() {
         return "Hello World";
     }
