ec1bc20d010c1ec7dfe6822b419010764e78ea3d
yue sy <1731719956@qq.com>
2024-10-11 20:14:02 +0800

    test

--- src/main/java/com/ghiv/ghivnew/controller/GenotypeController.java
+++ src/main/java/com/ghiv/ghivnew/controller/GenotypeController.java
@@ -3,11 +3,38 @@ package com.ghiv.ghivnew.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+
+import static org.apache.catalina.startup.ExpandWar.deleteDir;
+
 @RestController
 //鉴定分型①
 public class GenotypeController {
     @PostMapping("/genotype")
-    public String forwardToSecond() {
+    public String forwardToTimer7() {
+        return "forward:/timer7";
+    }
+    public void recreateDirectories() {
+        String currentDirectory = "";//修改为工作目录
+        deleteDir(new File(currentDirectory, "temp/temp_quick"));
+        new File(currentDirectory, "temp/temp_quick").mkdirs();
+
+        deleteDir(new File(currentDirectory, "temp/temp_data"));
+        new File(currentDirectory, "temp/temp_data").mkdirs();
+    }
+    public String forwardToFileUpload() {
         return "forward:/upload";
+    }
+
+    private void deleteDir(File dir) {
+        if (dir.isDirectory()) {
+            File[] children = dir.listFiles();
+            if (children != null) {
+                for (File child : children) {
+                    deleteDir(child);
+                }
+            }
+        }
+        dir.delete();
     }
 }
--- src/main/java/com/ghiv/ghivnew/controller/Timer7Controller.java
+++ src/main/java/com/ghiv/ghivnew/controller/Timer7Controller.java
@@ -3,5 +3,6 @@ package com.ghiv.ghivnew.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//更新进度条
 public class Timer7Controller {
 }
