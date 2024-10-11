e5e1122a25fe7f3ea8fddb46eb2b2da00c0bef73
yue sy <1731719956@qq.com>
2024-10-11 19:39:33 +0800

    test

--- src/main/java/com/ghiv/ghivnew/controller/FileUploadController.java
+++ src/main/java/com/ghiv/ghivnew/controller/FileUploadController.java
@@ -1,27 +1,19 @@
 package com.ghiv.ghivnew.controller;
 
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.lang.UUID;
-import cn.hutool.core.util.StrUtil;
-import cn.hutool.crypto.SecureUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
 @RestController
 public class FileUploadController {
     @PostMapping("/upload")
     public String up(String filename, MultipartFile file, HttpServletRequest request) throws IOException {
-
         String path = request.getServletContext().getRealPath("/upload");
         saveFile(file, path);
-
         return "File uploaded successfully";
     }
 
--- src/main/java/com/ghiv/ghivnew/controller/GenotypeController.java
+++ src/main/java/com/ghiv/ghivnew/controller/GenotypeController.java
@@ -1,8 +1,13 @@
 package com.ghiv.ghivnew.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //鉴定分型①
 public class GenotypeController {
+    @PostMapping("/genotype")
+    public String forwardToSecond() {
+        return "forward:/upload";
+    }
 }
